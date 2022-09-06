55e9b31422374ad9edccaac75c18cda751164bb1
Parker <parkerriddle09@gmail.com>
2022-09-06 14:12:22 -0600

    Got the notes to reappear if clicked

--- Develop/server.js
+++ Develop/server.js
@@ -1,65 +1,56 @@
+const { info } = require('console');
 const express = require('express');
 const path = require('path');
 const notes = require('./db/db.json');
-
-// const { clog } = require('./middleware/clog');
-// const api = require('./routes/index.js');
+const generateUniqueId = require('generate-unique-id');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
-// Import custom middleware, "cLog"
-// app.use(clog);
-
-// Middleware for parsing JSON and urlencoded form data
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-// app.use('/api', api);
 
 app.use(express.static('public'));
-
-app.get('/', (req, res) =>
-    res.sendFile(path.join(__dirname, '/public/index.html'))
-);
 
 app.get('/notes', (req, res) =>
     res.sendFile(path.join(__dirname, 'public/notes.html'))
 );
 
-app.get('/api/notes', (req, res) =>
+app.get('/api/notes', (req, res) => {
+    console.info(notes)
     res.json(notes)
-);
+});
 
-app.get('/api/notes/:id', (req, res) =>
+app.get('/api/notes/:id', (req, res) => {
     res.json(notes)
-);
+});
 
 app.post('/api/notes', (req, res) => {
-    console.info(req.body)
-    console.info(notes)
+
+    const uniqueId = generateUniqueId({
+        length: 32,
+        useLetters: false
+    });
+
+    req.body.id = uniqueId
     notes.push(req.body)
     console.info(notes)
     res.json(notes)
-    // let note;
-
-    // note = {
-    //     title: req.body.title,
-    //     text: req.body.text,
-    // };
 });
 
-// app.post('/api/notes', (req, res) =>
-//     req.body(notes)
-// );
+app.delete('/api/notes/:id', (req, res) => {
+    // console.info(notes[0])
+    // console.info(`req body:${req.body}`)
+    // let id = req.params.id
+    // console.info(id)
+    // res.json(notes[0])
+});
 
-// app.delete('/api/notes', (req, res) =>
+app.get('*', (req, res) =>
+    res.sendFile(path.join(__dirname, '/public/index.html'))
+);
 
-// );
-
-// app.delete('/api/notes/:id', (req, res) => (
-
-// );
 
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT} 🚀`)
